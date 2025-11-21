620ed3e012b9edcb2d88d822dca06739379fb111
RossDeVito <rdevito@ucsd.edu>
2025-11-21 15:16:24 -0800

    update train script

--- scripts/training/run_soft_prompt_training.sh
+++ scripts/training/run_soft_prompt_training.sh
@@ -4,7 +4,9 @@
 
 OUTPUT_DIR="scripts/training/output/soft_prompt"
 CONFIG_DIR="scripts/training/training_configs/soft_prompt"
-CONFIG_FILE="sp1_str2.yaml"
+CONFIG_FILE="sp_dev1_str2.yaml"
+
+# export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 python -m seq_models.hyenaDNA.train_soft_prompt \
 	--config "${CONFIG_DIR}/${CONFIG_FILE}" \
