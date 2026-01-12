40ce6caf5e9f9fa5393ab1c296f3aa157a359eeb
RossDeVito <rdevito@ucsd.edu>
2026-01-12 12:58:36 -0800

    script to evaluate and compare multiple models

---
+++ scripts/eval/eval_multiple_preds.py
@@ -0,0 +1,64 @@
+""" Evaluate predictions from multiple models. """
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn.metrics as sk_metrics
+import scipy.stats as stats
+from matplotlib.colors import LogNorm
+
+
+def get_metrics(pred, true):
+	mse = sk_metrics.mean_squared_error(true, pred)
+	rmse = np.sqrt(mse)
+	mae = sk_metrics.mean_absolute_error(true, pred)
+	r2 = sk_metrics.r2_score(true, pred)
+	pearson_r, pearson_p = stats.pearsonr(true, pred)
+	spearman_r, spearman_p = stats.spearmanr(true, pred)
+
+	return {
+		"MSE": mse,
+		"RMSE": rmse,
+		"MAE": mae,
+		"R2": r2,
+		"Pearson_r": pearson_r,
+		"Pearson_p": pearson_p,
+		"Spearman_r": spearman_r,
+		"Spearman_p": spearman_p,
+	}
+
+
+if __name__ == '__main__':
+
+	pred_dir = "predictions/soft_prompt/str2/tscc_dev"
+	model_names = [
+		"dev4Lb_2025-12-05_11-14-13",
+		"dev4Lb_m450_2025-12-06_16-44-44",
+		"dev5_m450_2025-12-08_02-35-32",
+		"dev5L_l1m_2025-12-08_00-22-52",
+		"dev5L_m450_2025-12-07_21-46-02",
+	]
+
+	results = []
+
+	for model_name in model_names:
+		pred_path = os.path.join(pred_dir, model_name, "predictions_test.tsv")
+
+		# Load predictions
+		pred_df = pd.read_csv(pred_path, sep="\t")
+
+		# Compute metrics
+		metrics = get_metrics(
+			pred=pred_df["pred_length"],
+			true=pred_df["true_length"]
+		)
+
+		results.append({
+			"model": model_name,
+			**metrics
+		})
+
+	results_df = pd.DataFrame(results)
+	print(results_df)
