c95c8fc8003da103d3035817ad6f7f269f22dfc1
RossDeVito <rdevito@ucsd.edu>
2026-06-22 12:57:25 -0700

    Add batch script to make predictions

--- scripts/eval_preds/make_predictions.sh
+++ scripts/eval_preds/make_predictions.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-MODEL_DIR="scripts/training/output/"
-OUTPUT_DIR="scripts/eval_preds/predictions/"
+MODEL_DIR="scripts/training/output/caduceus/"
+OUTPUT_DIR="scripts/eval_preds/predictions/caduceus/"
 
 # Check if an argument was provided
 if [ -z "$1" ]; then
 	echo "Error: No path to model savedir specified."
-	echo "Usage: $0 <model_savedir>"
+	echo "Usage: $0 <model_savedir> [split]"
 	echo " where <model_savedir> is a subdirectory of ${MODEL_DIR}"
+	echo " and [split] is one of test|val|train (default: test)"
 	exit 1
 fi
 
 # Assign the first argument ($1) to MODEL_SAVEDIR
 MODEL_SAVEDIR="$1"
+SPLIT="${2:-test}"
 echo "Making predictions with model in: ${MODEL_SAVEDIR}"
+echo "Split: ${SPLIT}"
 
 # Run the command
-python -m seq_models.hyenaDNA.predict \
+python -m seq_models.caduceus.predict \
 	--model_dir "${MODEL_DIR}/${MODEL_SAVEDIR}" \
-	--output_dir "${OUTPUT_DIR}/${MODEL_SAVEDIR}"
+	--output_dir "${OUTPUT_DIR}/${MODEL_SAVEDIR}" \
+	--split "${SPLIT}"
